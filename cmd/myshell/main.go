1bac20c7d5bc82d48e318884ea056f54dbe43a35
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2024-05-25 13:38:24 +0545

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,28 +7,43 @@ import (
 	"strings"
 )
 
+func isExecutablePath(cmd string) (string, bool) {
+	osPath := os.Getenv("PATH")
+	paths := strings.Split(osPath, ":")
+	for _, path := range paths {
+		if _, err := os.Stat(fmt.Sprintf("%s/%s", path, cmd)); err == nil {
+			return path,true
+		}
+	}
+	return "", false
+}
+
+func isBuiltin(name string) bool {
+	switch name {
+		case "exit","echo","type":
+			return true
+		default:
+			return false
+	}
+}
+
 func handelExecType(cmd string, input string){
 	response := ""
 	for _, word := range strings.Fields(input){
-		switch word {
-			case "exit","echo":
-				response = fmt.Sprintf("%s is a shell builtin\n", word)
-			case "cat":
-				response = fmt.Sprintf("%s is /bin/%s\n", word, word)
-			default:
-				response = fmt.Sprintf("%s not found\n", word)
+		if isBuiltin(word){
+			response = fmt.Sprintf("%s is a shell builtin\n", word)
+		}else if path, ok := isExecutablePath(word);ok {
+			response = fmt.Sprintf("%s is %s/%s\n", word, path, word)
+		}else{
+			response = fmt.Sprintf("%s not found\n", word)
 		}
-	}
-	if input == "type type"{
-		response = fmt.Sprintf("%s is a shell builtin\n", cmd)
-	}
+	}	
 	fmt.Printf("%s", response)
 }
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
-	const builtIns = "echo exit cat"
 	const prefixCommands = "echo type"
 	reader := bufio.NewReader(os.Stdin)
 	for {
