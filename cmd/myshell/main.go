7e7fcad0175bf6c187d47910fd09273a80be363a
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2024-05-25 13:58:00 +0545

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"os/exec"
 )
 
 func isExecutablePath(cmd string) (string, bool) {
@@ -27,9 +28,9 @@ func isBuiltin(name string) bool {
 	}
 }
 
-func handelExecType(cmd string, input string){
+func handelExecType(cmd string, args []string){
 	response := ""
-	for _, word := range strings.Fields(input){
+	for _, word := range args{
 		if isBuiltin(word){
 			response = fmt.Sprintf("%s is a shell builtin\n", word)
 		}else if path, ok := isExecutablePath(word);ok {
@@ -41,6 +42,24 @@ func handelExecType(cmd string, input string){
 	fmt.Printf("%s", response)
 }
 
+func handelExec(cmd string, args []string){
+	exec := exec.Command(cmd, args...)
+	exec.Stdout = os.Stdout
+	exec.Stderr = os.Stderr
+
+	err := exec.Run()
+	if err != nil {
+		fmt.Printf("%s: command not found\n", cmd)
+	}
+}
+
+func handelInput(input string)(cmd string, args []string){
+	args = strings.Fields(input)
+	cmd = args[0]
+	args = args[1:]
+	return cmd, args
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
@@ -49,26 +68,27 @@ func main() {
 	for {
 		// Uncomment this block to pass the first stage
 		fmt.Fprint(os.Stdout, "$ ")
-		cmd, _ := reader.ReadString('\n')
-		cmd = strings.TrimSpace(cmd)
-		input := cmd
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		inputCopy := input
+		cmd, args := handelInput(input)
 
 		for _, word := range strings.Fields(prefixCommands){
-			if strings.HasPrefix(cmd, fmt.Sprintf("%s ", word)){
-				cmd = word
+			if strings.HasPrefix(input, fmt.Sprintf("%s ", word)){
+				input = word
 				break;
 			}
 		}
 
-		switch cmd {
+		switch input {
 			case "exit 0":
 				os.Exit(0)
 			case "echo":
-				fmt.Printf("%s\n", strings.TrimPrefix(input, "echo "))
+				fmt.Printf("%s\n", strings.TrimPrefix(inputCopy, "echo "))
 			case "type":
-				handelExecType(cmd, input)
+				handelExecType(cmd, args)
 			default:
-				fmt.Printf("%s: command not found\n", cmd)
+				handelExec(cmd, args)
 		}
 
 	}
